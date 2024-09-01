578f33af2035017ce84530df5a7f662c32e8e9c6
Denis Gordeev <denis.gordeev@anec.app>
2024-09-01 15:39:32 +0400

    Refactor MangaOcr to support batch image processing with a custom dataset class.

--- manga_ocr/ocr.py
+++ manga_ocr/ocr.py
@@ -6,14 +6,43 @@ import torch
 from PIL import Image
 from loguru import logger
 from transformers import AutoFeatureExtractor, AutoTokenizer, VisionEncoderDecoderModel
+from torch.utils.data import DataLoader, Dataset
+
+
+class CustomImageDataset(Dataset):
+     def __init__(self, image_paths, feature_extractor):
+         self.image_paths = image_paths
+         self.feature_extractor = feature_extractor
+     
+     def __len__(self):
+         return len(self.image_paths)
+     
+     def __getitem__(self, idx):
+         img_paths = [self.image_paths[idx]]
+         new_imgs = []
+         for img_or_path in img_paths:
+            if isinstance(img_or_path, str) or isinstance(img_or_path, Path):
+                img = Image.open(img_or_path)
+            elif isinstance(img_or_path, Image.Image):
+                img = img_or_path
+            else:
+                raise ValueError(f'Invalid value of img_or_path: {img_or_path}')
+            new_imgs.append(img)
+         image_paths = new_imgs
+         imgs = [img.convert('L').convert('RGB') for img in image_paths]
+         image = self.feature_extractor(images=imgs, return_tensors="pt").pixel_values
+         image = image.squeeze()
+         return image
 
 
 class MangaOcr:
-    def __init__(self, pretrained_model_name_or_path='kha-white/manga-ocr-base', force_cpu=False):
+    def __init__(self, pretrained_model_name_or_path='kha-white/manga-ocr-base', force_cpu=False, batch_size=16):
         logger.info(f'Loading OCR model from {pretrained_model_name_or_path}')
         self.feature_extractor = AutoFeatureExtractor.from_pretrained(pretrained_model_name_or_path)
         self.tokenizer = AutoTokenizer.from_pretrained(pretrained_model_name_or_path)
+        logger.info(f'batch size: {batch_size}')
         self.model = VisionEncoderDecoderModel.from_pretrained(pretrained_model_name_or_path)
+        self.batch_size = batch_size
 
         if not force_cpu and torch.cuda.is_available():
             logger.info('Using CUDA')
@@ -21,32 +50,52 @@ class MangaOcr:
         else:
             logger.info('Using CPU')
 
-        example_path = Path(__file__).parent / 'assets/example.jpg'
-        if not example_path.is_file():
-            example_path = Path(__file__).parent.parent / 'assets/example.jpg'
-        self(example_path)
-
         logger.info('OCR ready')
 
-    def __call__(self, img_or_path):
-        if isinstance(img_or_path, str) or isinstance(img_or_path, Path):
-            img = Image.open(img_or_path)
-        elif isinstance(img_or_path, Image.Image):
-            img = img_or_path
-        else:
-            raise ValueError(f'Invalid value of img_or_path: {img_or_path}')
+    def __call__(self, imgs_or_paths):
+        if not isinstance(imgs_or_paths, list):
+            imgs_or_paths = [imgs_or_paths]
 
-        img = img.convert('L').convert('RGB')
+        dataset = CustomImageDataset(imgs_or_paths, self.feature_extractor)
+        dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=False)
+ 
 
-        x = self._preprocess(img)
-        x = self.model.generate(x[None].to(self.model.device))[0].cpu()
-        x = self.tokenizer.decode(x, skip_special_tokens=True)
-        x = post_process(x)
-        return x
+        images = []
+        for img_or_path in imgs_or_paths:
+            if isinstance(img_or_path, str) or isinstance(img_or_path, Path):
+                img = Image.open(img_or_path)
+            elif isinstance(img_or_path, Image.Image):
+                img = img_or_path
+            else:
+                raise ValueError(f'Invalid value of img_or_path: {img_or_path}')
+            img = img.convert('L').convert('RGB')
+            images.append(img)
 
-    def _preprocess(self, img):
-        pixel_values = self.feature_extractor(img, return_tensors="pt").pixel_values
-        return pixel_values.squeeze()
+        results = []
+        with torch.no_grad():
+            for batch in dataloader:
+                images = batch.to(self.model.device)
+                # Generate captions
+                outputs = self.model.generate(pixel_values=images, max_length=1000)
+
+                # Decode the generated captions
+                batch_captions = self.tokenizer.batch_decode(outputs, skip_special_tokens=True)
+                results.extend(batch_captions)
+
+        # results = []
+        # for i in range(0, len(images), self.batch_size):
+        #     batch = images[i:i + self.batch_size]
+        #     pixel_values = self._preprocess(batch)
+        #     generated_ids = self.model.generate(pixel_values.to(self.model.device), batch_size=self.batch_size)
+        #     texts = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+        #     texts = [post_process(text) for text in texts]
+        #     results.extend(texts)
+
+        return results
+
+    def _preprocess(self, imgs):
+        pixel_values = self.feature_extractor(imgs, return_tensors="pt").pixel_values
+        return pixel_values
 
 
 def post_process(text):
